93beb393b42c3285c019ee83011acce0e41edceb
schoenenp <schoenen@pirrot.de>
2024-05-08 04:05:28 +0200

    edit config to subdomain

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const NextConfig = {
     swcMinify: true,
     output: 'standalone',
     distDir: ".next",
-    assetPrefix: process.env.NODE_ENV === "production" ? `https://${process.env.NEXT_PUBLIC_CLIENT_APP_DOMAIN}` : `http://${process.env.NEXT_PUBLIC_CLIENT_DEV_DOMAIN}`
+    assetPrefix: process.env.NODE_ENV === "production" ? `https://secure.${process.env.NEXT_PUBLIC_CLIENT_APP_DOMAIN}` : `http://secure.${process.env.NEXT_PUBLIC_CLIENT_DEV_DOMAIN}:3000`
 
 };
 
